94b8913a21b3b3c14fd5c821d2155f146dd2e169
Tincho <martin.benedetto07@gmail.com>
2023-05-15 13:06:49 +0200

    switch de modo oscuro/claro

---
+++ js/action.js
@@ -0,0 +1,7 @@
+/*SWITCH*/
+const handle = document.querySelector('.handle');
+const icon = document.querySelector('.fa-solid');
+
+handle.addEventListener('click', ()=>{
+    handle.classList.toggle('move');
+})
