1496f5f911da8ce7fe1d6cddbd17b3bd49131e79
Tincho <martin.benedetto07@gmail.com>
2023-05-19 20:45:32 +0200

    segunda preEntrega lista

--- js/system.js
+++ js/system.js
@@ -1,3 +1,13 @@
+const dateFormat = (date)=>{
+    let splitDate = date.split('-');
+    let day = splitDate[2];
+    let month = splitDate[1];
+    let year = splitDate[0];
+
+    let format = day + '/' + month + '/' + year;
+    return format;
+}
+
 class Task{
     constructor(id, title, content, start, end) {
         this.id = id;
@@ -5,9 +15,10 @@ class Task{
         this.content = content;
         this.start = start;
         this.end = end;
+        this.checked = false;
     }
 }
-let taskList = [ new Task(0, 'PreEntrega2', 'Proyecto aprobado', '15/05/2023', '20/05/2023')];
+let taskList = [];
 
 const createTask = (title, content, start, end)=>{
     if(verificarVacio(content)){
@@ -23,16 +34,54 @@ const deleteTask = (id)=>{
     if(confirm('¿Deseas eliminar esta tarea?')){
         taskList.splice(id, 1);
         showTasks(taskList);
+        updatePercentage()
     }
 }
 
+const updatePercentage = ()=>{
+    const spanPercentage = document.querySelector('#percentage span');
+    if(listaVacia(taskList)){
+        spanPercentage.innerHTML = '';
+    }else{
+        let taskChecked = taskList.filter( task => task.checked );
+        let checkedAmount = taskChecked.length;
+        let totalTask = taskList.length;
+        let percentage = checkedAmount * 100 / totalTask;
+        spanPercentage.innerHTML = Math.round(percentage) + '%';
+    }
+}
+
+const checkTask = (id, cond)=>{
+    let task = taskList.find( (item)=> item.id == id );
+    task.checked = cond ? true : false;
+    updatePercentage();
+}
+
+const putCheck = (btn)=>{
+    let id = btn.getAttribute('data-id');
+    let task = taskList.find( task => task.id == id);
+    btn.checked = task.checked ? true : false;
+}
+
 const addTaskButtons = ()=>{
-    /*Delete*/
+    /*Delete button*/
     let deleteBtns = document.querySelectorAll('.delete-btn');
     deleteBtns.forEach(btn => {
         btn.addEventListener('click', ()=>{
             let id = btn.getAttribute('data-id');
             deleteTask(id);
+        })
+    });
+    /*Check button*/
+    let checkBtns = document.querySelectorAll('.checkbox');
+    checkBtns.forEach(btn => {
+        putCheck(btn);
+        btn.addEventListener('change', (event)=>{
+            if(event.target.checked){
+                checkTask(btn.getAttribute('data-id'), true);
+            }else{
+                checkTask(btn.getAttribute('data-id'), false);
+            }
         })
     });
 }
@@ -52,13 +101,13 @@ const showTasks = (taskList)=>{
                     <p class="task-text s-text">${task.content}</p>
                     <button class="info-btn"><i class="fa-solid fa-circle-info"></i></button>
                     <div class="task-dates">
-                        <p class="xs-text"><span>Inicio: ${task.start}</span></p>
-                        <p class="xs-text"><span>Fin: ${task.end}</span></p>
+                        <p class="xs-text"><span>Inicio: ${dateFormat(task.start)}</span></p>
+                        <p class="xs-text"><span>Fin: ${dateFormat(task.end)}</span></p>
                     </div>
                 </div>
                 <div class="task-btns">
-                    <input type="checkbox" name="" id="">
-                    <span class="edit-btn"><i class="fa-solid fa-pencil"></i></span>
+                    <input type="checkbox" class="checkbox" data-id="${task.id}">
+                    <span class="edit-btn" data-id="${task.id}"><i class="fa-solid fa-pencil"></i></span>
                     <span class="delete-btn" data-id="${task.id}"><i class="fa-solid fa-trash"></i></span>
                 </div>
             </div>
@@ -70,18 +119,56 @@ const showTasks = (taskList)=>{
 }
 
 const searchTask = (search)=>{
-    let searchListTask = new Array();
-    let taskSearched = taskList.find( (task)=> task.title === search );
-    if(taskSearched === undefined){
+    let searchListTask = taskList.filter( (task)=> task.title === search );
+    if(listaVacia(searchListTask)){
         alert('No se encontró la tarea solicitada!');
     }else{
-        searchListTask.push(taskSearched);
         showTasks(searchListTask);
     }
 }
 
-const sortTask = ()=>{
+const sortTask = (filtro)=>{
+    let date = new Date();
+    if(filtro === 'hoy'){
+        let day = date.getDate().toString().padStart(2, "0");
+        let month = (date.getMonth() + 1).toString().padStart(2, "0");
+        let year = date.getFullYear().toString();
+        let today = year + '-' + month + '-' + day;
+        let filterTaskList = taskList.filter((task)=> task.start === today);
 
+        showTasks(filterTaskList);
+    }else if(filtro === 'semana'){
+        //Fecha de hoy
+        let today = date.getDay();
+
+        //Fecha del primer día de la semana (Lunes)
+        const firstDay = new Date(date);
+        firstDay.setDate(date.getDate() - today);
+        
+        //Fecha del último día de la semana (Domingo)
+        const lastDay = new Date(date);
+        lastDay.setDate(date.getDate() + (7 - today));
+
+        let filterTaskList = taskList.filter((task)=> {
+            let taskDate = new Date(task.start)
+            return taskDate >= firstDay && taskDate <= lastDay;
+        });
+
+        showTasks(filterTaskList);
+    }else if(filtro === 'mes'){
+        let thisMonth = (date.getMonth() + 1).toString().padStart(2, "0");
+        let thisYear = date.getFullYear().toString();
+        let thisMonthYear = thisYear + "-" + thisMonth;
+ 
+        let filterTaskList = taskList.filter((task)=> {
+            let taskMonth = task.start.slice(0, 7);
+            return taskMonth === thisMonthYear;
+        })
+
+        showTasks(filterTaskList);
+    }else{
+        showTasks(taskList);
+    }
 }
 
 //VERIFICADORES
@@ -92,6 +179,9 @@ const listaVacia = (lista)=>{
     return !lista || lista.length === 0;
 }
 
+//-------------------------------------------------------------------------------
+
+/*INTERFAZ PARA AGREGAR TAREA*/
 const addBtn = document.querySelector('.add-btn');
 const addTaskSection = document.querySelector('.add-task-section');
 
@@ -99,73 +189,52 @@ addBtn.addEventListener('click', ()=>{
     addTaskSection.classList.add('showAddTask');
 })
 
-const taskTitle = document.querySelector('#taskTitle');
-const taskContent = document.querySelector('#taskContent');
-const taskStart = document.querySelector('#taskStart');
-const taskEnd = document.querySelector('#taskEnd');
-
-let title = 'Sin título';
-let content, start, end, flag = 0;
-
-taskTitle.addEventListener('click', ()=>{
-    title = '';
-    title = prompt('Ingrese el titulo:');
-})
-taskContent.addEventListener('click', ()=>{
-    content = '';
-    content = prompt('Ingrese su tarea:');
-    flag++;
-})
-taskStart.addEventListener('click', ()=>{
-    start = '';
-    start = prompt('Ingrese la fecha de inicio (dia/mes/año):');
-    flag++;
-})
-taskEnd.addEventListener('click', ()=>{
-    end = '';
-    end = prompt('Ingrese la fecha de finalización (dia/mes/año):');
-    flag++;
-})
-
-const confirmTask = document.querySelector('#confirmTask');
+//Botones para enviar o cancelar el agregado de una tarea
+const taskForm = document.querySelector('#task-form');
 const cancelTask = document.querySelector('#cancelTask');
 
 cancelTask.addEventListener('click', ()=>{
     addTaskSection.classList.remove('showAddTask');
 })
-confirmTask.addEventListener('click', ()=>{
-    if(flag >= 3){
-        createTask(title, content, start, end);
-        addTaskSection.classList.remove('showAddTask');
-    }else{
-        alert('Complete los campos requeridos!');
-    }
+
+//Envio del formulario (tarea)
+taskForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    let taskTitle = document.querySelector('#taskTitle').value;
+    let taskContent = document.querySelector('#taskContent').value;
+    let taskStart = document.querySelector('#taskStart').value;
+    let taskEnd = document.querySelector('#taskEnd').value;
+    createTask(taskTitle, taskContent, taskStart, taskEnd);
+    addTaskSection.classList.remove('showAddTask');
+    taskForm.reset();
 })
 
 /*BUSCADOR*/
-const searchInput = document.querySelector('#searchInput');
+const searchBar = document.querySelector('.searchBar');
 const lupa = document.querySelector('.fa-magnifying-glass');
+const searchInput = document.querySelector('#searchInput');
+const searchBtn = document.querySelector('#searchBtn');
 
-lupa.addEventListener('click', ()=>{
-    searchInput.classList.toggle('showInput');
-})
-searchInput.addEventListener('click', ()=>{
-    let search = prompt('Ingrese la tarea que busca por su titulo:');
-    console.log(search)
+/*lupa.addEventListener('click', ()=>{
+    searchBar.classList.toggle('showSearchBar');
+})*/
+searchBtn.addEventListener('click', ()=>{
+    let search = searchInput.value;
     if(search === '' || search === null || search === 'todo'){
         showTasks(taskList);
     }else{
         searchTask(search);
     }
+    searchInput.value = '';
 })
 
 /*FILTRO*/
-const arrow = document.querySelector('.fa-caret-down');
 const sortSelect = document.querySelector('#sortSelect');
 
-sortSelect.addEventListener('click', ()=>{
-    arrow.classList.toggle('spin90');
+sortSelect.addEventListener('change', ()=>{
+    sortTask(sortSelect.value);
 })
 
 /*INICIO*/
 showTasks(taskList);
+updatePercentage();
